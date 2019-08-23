ab7b2bbd364e5da8d98c24bc3c63cc8157f66163
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-08-23 17:57:15 +0800

    homepage cards grid

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,13 +1,32 @@
-import { Breadcrumb, Layout } from 'antd'
+import { Breadcrumb, Card,Col, Layout,Row } from 'antd'
 import React from 'react'
+import './index.less'
 
 const Homepage = (): React.ReactNode => {
   return (
-    <Layout.Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>User</Breadcrumb.Item>
-        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-      </Breadcrumb>
+    <Layout.Content className='hl-page-wrapper'>
+      <Row gutter={16} className='stats-row'>
+        <Col span={6}>
+          <Card title='Card title' bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title='Card title' bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title='Card title' bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title='Card title' bordered={false}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
       <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
         This is homepage.
       </div>
