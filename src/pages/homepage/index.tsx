e7de05aeae7cad52dcca123ba5f2495ecf7d982c
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-08-27 12:05:34 +0800

    event list

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Layout, Row, Statistic } from 'antd'
+import { Avatar, Card, Col, Layout, List, Row, Statistic, Tag } from 'antd'
 import React from 'react'
 import {
   Area,
@@ -10,6 +10,13 @@ import {
   XAxis
 } from 'recharts'
 import './index.less'
+
+interface IEvent {
+  content: string
+  time: string
+  type: string
+  user: string
+}
 
 const Homepage = (): React.ReactNode => {
   const dataYearElecUse = [
@@ -133,8 +140,56 @@ const Homepage = (): React.ReactNode => {
     }
   ]
 
+  const eventList = [
+    {
+      content: '当前剩余金额【-930392.78元】<= 拉闸金额【0.00元】',
+      time: '2019-08-27 08:00:00',
+      type: '预付费拉闸',
+      user: '元麦汽车'
+    },
+    {
+      content: '当前剩余金额【-0.14元】<= 拉闸金额【0.00元】',
+      time: '2019-08-27 08:00:00',
+      type: '预付费拉闸',
+      user: '孙迪'
+    },
+    {
+      content: '当前剩余金额【0.01元】<= 拉闸金额【0.00元】',
+      time: '2019-08-27 08:00:00',
+      type: '预付费拉闸',
+      user: '#2演示箱'
+    },
+    {
+      content: '当前剩余金额【0.00元】<= 拉闸金额【0.00元】',
+      time: '2019-08-27 08:00:00',
+      type: '预付费拉闸',
+      user: '演示箱#2专用'
+    },
+    {
+      content: '当前剩余金额【0.00元】<= 拉闸金额【0.00元】',
+      time: '2019-08-27 08:00:00',
+      type: '预付费拉闸',
+      user: '导轨表'
+    }
+  ]
+
   const usageFormatter = (value: number) => [value, '月用量']
   const costFormatter = (value: number) => [value, '月费用']
+  const eventListRenderer = (item: IEvent) => (
+    <List.Item extra={item.time}>
+      <List.Item.Meta
+        avatar={
+          <Avatar src='http://resource.energyman.cn/data//res/img/fee/event_disconnect.png' />
+        }
+        title={item.user}
+        description={
+          <div>
+            <Tag color='blue'>{item.type}</Tag> {item.content}
+          </div>
+        }
+      />
+    </List.Item>
+  )
 
   const statsValueStyle = { fontSize: '30px' }
   const cardBodyStyle = { padding: '20px 24px 8px' }
@@ -143,7 +198,7 @@ const Homepage = (): React.ReactNode => {
 
   return (
     <Layout.Content className='hl-page-wrapper'>
-      <Row gutter={16} className='stats-row'>
+      <Row gutter={16} className='home-row'>
         <Col span={6}>
           <Card bodyStyle={cardBodyStyle} bordered={false}>
             <Statistic
@@ -267,28 +322,48 @@ const Homepage = (): React.ReactNode => {
           </Card>
         </Col>
       </Row>
-      <Card title='运行工况统计' bordered={false}>
-        <Row>
-          <Col span={4}>
-            <Statistic title='用户总数' value={18} />
-          </Col>
-          <Col span={4}>
-            <Statistic title='已欠费数' value={4} suffix={`/ ${18}`} />
-          </Col>
-          <Col span={4}>
-            <Statistic title='可催费数' value={0} suffix={`/ ${18}`} />
-          </Col>
-          <Col span={4}>
-            <Statistic title='未按时上报数' value={200}  />
-          </Col>
-          <Col span={4}>
-            <Statistic title='催费次数' value={6}  />
-          </Col>
-          <Col span={4}>
-            <Statistic title='拉闸次数' value={52}  />
-          </Col>
-        </Row>
-      </Card>
+      <Row className='home-row'>
+        <Col>
+          <Card title='运行工况统计' bordered={false}>
+            <Row>
+              <Col span={4}>
+                <Statistic title='用户总数' value={18} />
+              </Col>
+              <Col span={4}>
+                <Statistic title='已欠费数' value={4} suffix={`/ ${18}`} />
+              </Col>
+              <Col span={4}>
+                <Statistic title='可催费数' value={0} suffix={`/ ${18}`} />
+              </Col>
+              <Col span={4}>
+                <Statistic title='未按时上报数' value={200} />
+              </Col>
+              <Col span={4}>
+                <Statistic title='催费次数' value={6} />
+              </Col>
+              <Col span={4}>
+                <Statistic title='拉闸次数' value={52} />
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+      <Row className='home-row'>
+        <Col>
+          <Card
+            title='告警事件'
+            bordered={false}
+            bodyStyle={{ padding: '8px 24px' }}
+          >
+            <List
+              itemLayout='horizontal'
+              dataSource={eventList}
+              pagination={{ position: 'bottom' }}
+              renderItem={eventListRenderer}
+            />
+          </Card>
+        </Col>
+      </Row>
     </Layout.Content>
   )
 }
