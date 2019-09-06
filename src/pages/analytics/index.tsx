89046dde9325b1675e35ee0a98194d119c897034
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-09-06 17:30:58 +0800

    analytics init

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -1,17 +1,29 @@
-import { Breadcrumb, Layout } from 'antd'
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import PageHolder from '../../components/page-holder'
+import './index.less'
+
+const CompPowerFactor = lazy(() => import('./power-factor'))
+const CompPayload = lazy(() => import('./payload'))
+const CompGraph = lazy(() => import('./graph'))
+const CompThreePhases = lazy(() => import('./three-phases'))
+const CompBalance = lazy(() => import('./balance'))
+const CompAlert = lazy(() => import('./alert'))
+const CompNotFound = lazy(() => import('../../pages/not-found'))
 
 const PageAnalytics: React.FunctionComponent = (): JSX.Element => {
   return (
-    <Layout.Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>User</Breadcrumb.Item>
-        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-        This is analytics.
-      </div>
-    </Layout.Content>
+    <Suspense fallback={<PageHolder />}>
+      <Switch>
+        <Route path='/analytics/power-factor' component={CompPowerFactor} />
+        <Route path='/analytics/payload' component={CompPayload} />
+        <Route path='/analytics/graph' component={CompGraph} />
+        <Route path='/analytics/three-phases' component={CompThreePhases} />
+        <Route path='/analytics/balance' component={CompBalance} />
+        <Route path='/analytics/alert' component={CompAlert} />
+        <Route component={CompNotFound} />
+      </Switch>
+    </Suspense>
   )
 }
 
